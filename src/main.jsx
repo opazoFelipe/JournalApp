0bcd536f3adcced1e254c2603d2d122c0f521dd4
Felipe <Felipe.orivas@gmail.com>
2022-09-06 16:46:13 -0300

    crea store y authSlice

--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,19 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './styles.css'
+import { Provider } from 'react-redux'
 import { BrowserRouter } from 'react-router-dom'
 
 import { JournalApp } from './JournalApp'
+import { store } from './store';
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
-        <BrowserRouter>
-            <JournalApp />
-        </BrowserRouter>
+        <Provider store={ store }>
+            <BrowserRouter>
+                <JournalApp />
+            </BrowserRouter>
+        </Provider>
     </React.StrictMode>
 )
